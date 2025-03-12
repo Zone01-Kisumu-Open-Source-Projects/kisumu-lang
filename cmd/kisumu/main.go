d6d614029e9c6f5b6472a56c836e48e7117a4e56
johneliud <johneliud4@gmail.com>
2025-03-12 12:14:57 +0300

    refactor: initialize InitLogger() that specifies the path and level for log values.

--- cmd/kisumu/main.go
+++ cmd/kisumu/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"log/slog"
+
+	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/pkg/logger"
 )
 
 func main() {
+	// Default log level assumed to be info
+	level := slog.LevelInfo
+	err := logger.InitLogger("app.log", level)
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+
 	fmt.Println("Kisumu Lang CLI")
 }
