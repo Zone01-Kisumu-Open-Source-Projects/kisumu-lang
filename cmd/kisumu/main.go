a6594836bb33fb8d035ed7a285dd83f5f5295663
johneliud <johneliud4@gmail.com>
2025-03-12 12:32:06 +0300

    refactor: modify code to create a new data directory where app.log file will be created.

--- cmd/kisumu/main.go
+++ cmd/kisumu/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"log/slog"
+	"os"
 
 	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/pkg/logger"
 )
 
 func main() {
+	_ = os.Mkdir("data", 0744)
+
 	// Default log level assumed to be info
 	level := slog.LevelInfo
-	err := logger.InitLogger("app.log", level)
+	err := logger.InitLogger("data/app.log", level)
 	if err != nil {
 		log.Fatalf("Failed to initialize logger: %v", err)
 	}
 
-	fmt.Println("Kisumu Lang CLI")
+	logger.Info("Kisumu Lang CLI")
 }
--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"log/slog"
+	"os"
 
 	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/internal/repl"
+	"github.com/Zone01-Kisumu-Open-Source-Projects/kisumu-lang/pkg/logger"
 )
 
 func main() {
+	_ = os.Mkdir("data", 0o744)
+
+	// Default log level assumed to be info
+	level := slog.LevelInfo
+	err := logger.InitLogger("data/app.log", level)
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+
 	file := flag.String("file", "", "File to read input from")
 	flag.Parse()
 
 	if *file != "" {
 		repl.ReadFile(*file)
 	} else {
-		fmt.Println("No file provided. Starting REPL...")
+		logger.Warn("No file provided. Starting REPL...")
 		repl.Start()
 	}
 }
